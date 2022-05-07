8e901191f922ee7c34662235b6dd230918b146e7
Jayadev-3 <jayadevpu3@gmail.com>
2022-05-07 23:34:02 +0530

    RatingBar App directory

---
+++ RatingBar App/app/src/main/java/com/example/ratingbar/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ratingbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    RatingBar r1;
+    TextView t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        r1=findViewById(R.id.ratingBar);
+        t1=findViewById(R.id.text);
+
+        r1.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                t1.setText("Rating : "+v);
+            }
+        });
+    }
+}
